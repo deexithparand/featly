e2b1e1ed1a9f4501b410c858337e331b3d8b2294
deexithparand <deexith2002@gmail.com>
2025-03-31 23:20:46 +0530

    feat : root handler

--- server/server.go
+++ server/server.go
@@ -8,6 +8,11 @@ import (
 	"github.com/go-chi/chi"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome to Featly"))
+}
+
 func StartServer() {
 
 	// load config
@@ -18,6 +23,9 @@ func StartServer() {
 
 	// server setup
 	router := chi.NewRouter()
+
+	router.Get("/", rootHandler)
+
 	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		panic(err)
