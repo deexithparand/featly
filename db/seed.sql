1b4881ff48465aec6053b2e059ef8830bf6a6863
deexijman <deexith@jmangroup.com>
2025-04-01 19:52:21 +0530

    fix : updated the seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -13,11 +13,11 @@ INSERT INTO clients (client_id, client_name, subdomain) VALUES
 -- Insert Features (Retrieve valid `client_id` dynamically)
 INSERT INTO feature_requests (feature_id, feature_title, feature_details, upvote_count, client_id)
 VALUES
-(uuid_generate_v4(), 'Dark Mode', "Dark mode isn't working on home page", 5, (SELECT id FROM clients WHERE subdomain = 'atb')),
-(uuid_generate_v4(), 'Export to CSV', "Export functionality working for excel and not for CSV", 2, (SELECT id FROM clients WHERE subdomain = 'bof'));
+(uuid_generate_v4(), 'Dark Mode', 'Dark mode is not working on home page', 5, (SELECT client_id FROM clients WHERE subdomain = 'atb')),
+(uuid_generate_v4(), 'Export to CSV', 'Export functionality working for excel and not for CSV', 2, (SELECT client_id FROM clients WHERE subdomain = 'bof'));
 
 -- Insert Tags (Retrieve valid `feature_id` dynamically)
 INSERT INTO feature_tags (tag_id, tag_name, feature_id)
 VALUES
-(uuid_generate_v4(), 'Pending', (SELECT id FROM features WHERE feature = 'Dark Mode')),
-(uuid_generate_v4(), 'Popular', (SELECT id FROM features WHERE feature = 'Export to CSV'));
+(uuid_generate_v4(), 'Pending', (SELECT feature_id FROM feature_requests WHERE feature_title = 'Dark Mode')),
+(uuid_generate_v4(), 'Popular', (SELECT feature_id FROM feature_requests WHERE feature_title = 'Export to CSV'));
